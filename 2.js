7ad13e86f0ac03d187c988bdb63ad4db2f1da954
Vedanshi0908 <mesvaniya.vedanshi@gmail.com>
2025-07-14 10:53:51 +0530

    basic staterd file add

--- 2.js
+++ 2.js
@@ -1,1 +1,9 @@
-//Find maximum of 
+//Find maximum of two numbers
+const a = 10;
+const b = 20;
+if (a > b) {
+    console.log(`${a} is greater than ${b}`);
+}   
+else {
+    console.log(`${b} is greater than ${a}`);
+}
