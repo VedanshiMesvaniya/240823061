397c605c975d0f0e665841f2f8780378707c330d
Vedanshi0908 <mesvaniya.vedanshi@gmail.com>
2025-07-14 11:40:13 +0530

    added

--- 7.js
+++ 7.js
@@ -1,10 +1,29 @@
 // Reverse a Number
 
-const num = 1234;
-let rev = 0;
+// let num = 1234;
+// let rev = 0;
 
-while(num > 0){
-    const digit = num % 10;
-    rev = (rev * 10) + digit;
-    num = Math.floor(num / 10);
+// while (num > 0) {
+//     let digit = num % 10;
+//     console.log("digit:", digit);
+//     rev = rev * 10 + digit;
+//     num = Math.floor(num / 10);
+// }
+// console.log(rev);
+
+
+
+// reverseNumber.js
+
+let number = 1234;
+let reverse = 0;
+
+while (number > 0) {
+    let digit = number % 10;     
+    // console.log("Digit:", digit);         
+    reverse = reverse * 10 + digit; 
+    // console.log("Reverse so far:", reverse);       
+    number = Math.floor(number / 10);      
 }
+
+console.log(reverse);
